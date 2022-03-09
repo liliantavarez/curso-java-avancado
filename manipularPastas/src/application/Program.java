b467e300ae92382c5df5238fa2ddf5bca5e5607a
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2022-03-09 12:54:57 -0300

    manipulação de arquivos e pastas

---
+++ manipularPastas/src/application/Program.java
@@ -0,0 +1,36 @@
+package application;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Program {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter a folder path: ");
+		String strPath = sc.nextLine();
+
+		File path = new File(strPath);
+
+		File[] folders = path.listFiles(File::isDirectory);
+		System.out.println("FOLDERS: ");
+		for (File folder : folders) {
+			System.out.println(folder);
+		}
+		
+		File[] files = path.listFiles(File::isFile);
+		System.out.println("FILES: ");
+		for (File file : files) {
+			System.out.println(file);
+			System.out.println("Name: "+file.getName());
+			System.out.println("Parent: "+file.getParent());
+			System.out.println("Path: "+file);
+		}
+
+		boolean success = new File(strPath + "\\newPast").mkdir();
+		System.out.println("Diretorio criado com sucesso: "+success);
+
+		sc.close();
+	}
+}
